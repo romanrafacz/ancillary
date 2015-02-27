fe6a46b48ab1bfaf8ba18df12611b1375f67c55c
shoemonkey <romanr32@gmail.com>
2015-02-27 19:27:33 +0100

    request base overhauled, accordian working

--- request/forms.py
+++ request/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from request.models import Room_Details
+from functools import partial
+from django import forms
+DateInput = partial(forms.DateInput, {'class': 'datepicker'})
 
 class RoomDetailsForm(ModelForm):
+    
+
     class Meta:
         model = Room_Details
         fields = ['meeting_title', 'request_date', 'location_name', 'room_name', 'location_address', 'start_date', 'end_date', 'start_time', 'end_time', 'attendance', 'est_budget', 'setup_style']
+        widgets = { 'start_date': DateInput()}
 
