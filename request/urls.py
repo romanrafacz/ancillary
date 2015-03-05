01b7367091c8d75a022f7e128be5b85e6492437b
shoemonkey <romanr32@gmail.com>
2015-03-05 02:29:49 +0100

    index overhaul for portal/manager

--- request/urls.py
+++ request/urls.py
@@ -5,6 +5,7 @@ from request import views
 urlpatterns = patterns('',
     url(r'^$', views.index),
     url(r'^submit_request/$', views.submit_request, name="submit_request"),
+    url(r'^new_request/$', views.new_request, name="new_request"),
 
 
 )
--- request/views.py
+++ request/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from request.forms import RoomDetailsForm
+from request.models import Room_Details
 
 # Create your views here.
 
@@ -9,4 +11,16 @@ def index(request):
     return render(request, 'request/index.html', {'room_form':room_form})
 
 def submit_request(request):
-    return render(request, 'request/submit_success.html', {})
+    room_form = RoomDetailsForm(data=request.POST)
+    requested_data = Room_Details.objects.all()
+    if request.method == "POST" and room_form.is_valid():
+        room_data = Room_Details(meeting_title=request.POST['meeting_title'], request_date=request.POST['request_date'], location_name=request.POST['location_name'], room_name=request.POST['room_name'], location_address=request.POST['location_address'], start_date=request.POST['start_date'], end_date=request.POST['end_date'], start_time=request.POST['start_time'], end_time=request.POST['end_time'], attendance=request.POST['attendance'], est_budget=request.POST['est_budget'], setup_style=request.POST['setup_style']) 
+        room_data.save()
+        return render(request, 'request/submit_success.html', {'requested_data':requested_data})
+    else:
+        return HttpResponse('something off')
+
+    #return render(request, 'request/index.html', {'room_form':room_form})
+
+def new_request(request):
+    return render(request, 'request/new_request_page.html', {})
